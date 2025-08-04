ec3fc872e717c54282b4a924a7d9d01324daf882
Daniee19 <F4r3ver@gmail.com>
2025-08-03 22:17:56 -0500

    feat: Se completó con el proyecto. Además que creó y se mejoró el README.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Challenge Amigo Secreto - Alura Latam
+<p align="center">
+<img src="https://img.shields.io/badge/STATUS-TERMINADO-green">
+</p>
+
+![GitHub Org's stars](https://img.shields.io/github/stars/Daniee19?style=social)
+
+Proyecto terminado para el curso Practicando Lógica de Programación: Challenge Amigo secreto. Es un proyecto básico para reforzar conocimientos vistos en el curso de Javascript. <br/>
+Este proyecto permite agregar y visualizar los nombres ingresados por el usuario, validando previamente si el input tiene algun valor o no. Además cuando el usuario presione el botón "Sortear amigo", se escogerá un valor aleatorio del array donde se agregaron los nombres.
+
+## Autor
+<p align="left">
+  <a href="https://github.com/Daniee19">
+    <img src="https://avatars.githubusercontent.com/u/142465643?v=4" width="115">
+    <br>
+    <sub><b>Daniel Castañeda</b></sub>
+  </a>
+</p>
+
--- app.js
+++ app.js
@@ -1,1 +1,43 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let amigo = document.getElementById("amigo").value;
+
+    if (amigo === null || amigo === "") {
+        return alert("Por favor, inserte un nombre.");
+    }
+    amigos.push(amigo);
+
+    document.getElementById("amigo").value = "";
+
+    actualizarVista();
+}
+
+function actualizarVista() {
+    let listaAmigos = document.getElementById("listaAmigos");
+    //Para evitar duplicados
+    listaAmigos.innerHTML = "";
+
+    for (a of amigos) {
+        let item = document.createElement("li");
+        item.textContent = a;
+
+        listaAmigos.appendChild(item);
+    }
+
+}
+
+function sortearAmigo() {
+    if (amigos === null || amigos == []) {
+        return alert("Por favor, ingresa nombres para poder sortearlos.");
+    }
+
+    let indiceAleatorio = Math.floor((Math.random() * amigos.length));
+
+    let valorAleatorio = amigos[indiceAleatorio];
+
+    document.getElementById("resultado").innerHTML = valorAleatorio;
+
+
+}
